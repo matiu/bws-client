d116f7c8c4a9d0a8140e3a74ad9f1c697a433529
Gustavo Maximiliano Cortez <cmgustavo83@gmail.com>
2015-03-04 02:13:14 -0300

    bitcore-wallet-client dependecies

--- bws-client.js
+++ bws-client.js
@@ -1,14 +1,14 @@
 var express = require('express');
-var app = express();                                  // create our app w/ express
-var bodyParser = require('body-parser');          // pull information from HTML POST (express4)
+var app = express();
+var bodyParser = require('body-parser');
 var path = require('path');
 var recursive = require('recursive-readdir');
-var Client = require('./client/index.js');
+var Client = require('./node_modules/bitcore-wallet-client/lib/index.js');
 
-app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
-app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
-app.use(bodyParser.json());                                     // parse application/json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
 var port = process.env.BWSCLIENT_PORT || 5000;
 app.listen(port);
